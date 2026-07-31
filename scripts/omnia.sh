7c2ce49cc0b8df7d186bf9617e92903f0df75e67
raven <raven@trixie-omnia.hsd1.ma.comcast.net>
2026-07-31 00:24:00 -0400

    Fri Jul 31 12:24:00 AM EDT 2026

--- scripts/omnia.sh
+++ scripts/omnia.sh
@@ -2,14 +2,13 @@
 
 # Description: This script sets my main host machine running Debian 13+ with all of the tools needed. 
 
+
 # Add packages that needs to be installed
-PKGS="wget curl git brave-browser ffmpeg qtile yt-dlp "
+PKGS="wget curl git brave-browser ffmpeg yt-dlp openssh-server"
 
-echo "Updating package index..."
 sudo apt update
-echo "Package index updated."
 
-echo "Installing packages: $PKGS"
+echo "Installing packages..."
 sudo apt install "$PKGS"
 
 sudo apt upgrade -y
@@ -17,14 +16,15 @@ sudo apt upgrade -y
 echo "Installing starship prompt"
 curl -sS https://starship.rs/install.sh | sh
 
-# Get dotfiles from remote repo
+echo "Installing Docker..."
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+#echo "Installing Antigravity CLI"
+#curl -fsSL https://antigravity.google/cli/install.sh | bash
 
 
-
-echo "Installing Antigravity CLI"
-curl -fsSL https://antigravity.google/cli/install.sh | bash
-
-echo "Omnia installation is complete."
+echo -e "\e[1;32mOmnia installation is complete.\e[0m"
 
 
 
